ef8d85a86463e0058196da412669dd9e9db0a333
GalouM <galou.minisini@gmail.com>
2019-05-12 21:35:32 -0700

    clsoing time done

--- Go4Lunch/app/src/main/java/com/galou/go4lunch/restaurantsList/RestaurantViewHolder.java
+++ Go4Lunch/app/src/main/java/com/galou/go4lunch/restaurantsList/RestaurantViewHolder.java
@@ -1,17 +1,22 @@
 package com.galou.go4lunch.restaurantsList;
 
 import android.content.res.Resources;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.widget.TextViewCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.RequestManager;
 import com.galou.go4lunch.R;
 import com.galou.go4lunch.models.Restaurant;
 import com.galou.go4lunch.util.OpeningHoursUtil;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by galou on 2019-05-09
@@ -25,6 +30,8 @@ public class RestaurantViewHolder extends RecyclerView.ViewHolder {
     private TextView distance;
     private ImageView imageView;
     private Resources res;
+    
+    private String formatTimeDisplay; 
 
     public RestaurantViewHolder(@NonNull View itemView) {
         super(itemView);
@@ -35,6 +42,8 @@ public class RestaurantViewHolder extends RecyclerView.ViewHolder {
         openingHours = (TextView) itemView.findViewById(R.id.opening_hours_resto);
         distance = (TextView) itemView.findViewById(R.id.distance_resto);
         imageView = (ImageView) itemView.findViewById(R.id.image_resto);
+        formatTimeDisplay = res.getString(R.string.format_time_display);
+
     }
 
     public void updateRestaurantInfo(Restaurant restaurant, RequestManager glide){
@@ -58,18 +67,29 @@ public class RestaurantViewHolder extends RecyclerView.ViewHolder {
         switch (timeOpening){
             case R.string.closed:
                 openingHours.setText(timeOpening);
+                TextViewCompat.setTextAppearance(openingHours, R.style.TimeRestaurantClosed);
                 break;
             case R.string.open_until:
-                openingHours.setText(String.format(res.getString(timeOpening), restaurant.getClosureHours()));
+                DateFormat dateFormat = new SimpleDateFormat(formatTimeDisplay);
+                String timeToDisplay = dateFormat.format(OpeningHoursUtil.convertStringInDate(restaurant.getClosureHours()));
+                openingHours.setText(String.format(res.getString(timeOpening), timeToDisplay));
+                TextViewCompat.setTextAppearance(openingHours, R.style.TimeRestaurantOpen);
                 break;
             case R.string.opening_soon:
                 openingHours.setText(timeOpening);
+                TextViewCompat.setTextAppearance(openingHours, R.style.TimeRestaurantClosingSoon);
                 break;
             case R.string.closing_soon:
                 openingHours.setText(timeOpening);
+                TextViewCompat.setTextAppearance(openingHours, R.style.TimeRestaurantClosingSoon);
                 break;
             case R.string.no_time:
                 openingHours.setText(timeOpening);
+                TextViewCompat.setTextAppearance(openingHours, R.style.TimeRestaurantOpen);
+                break;
+            case R.string.open_24h:
+                openingHours.setText(timeOpening);
+                TextViewCompat.setTextAppearance(openingHours, R.style.TimeRestaurantOpen);
                 break;
         }
 
--- Go4Lunch/app/src/main/java/com/galou/go4lunch/util/OpeningHoursUtil.java
+++ Go4Lunch/app/src/main/java/com/galou/go4lunch/util/OpeningHoursUtil.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.galou.go4lunch.R;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -16,43 +17,44 @@ import java.util.concurrent.TimeUnit;
  */
 public abstract class OpeningHoursUtil {
 
-    private static String FORMAT_HOURS = "hhmm";
+    private static String FORMAT_HOURS = "HHmm";
 
     public static int getOpeningText(String openingHours, String closureHours){
+        int opening = Integer.parseInt(openingHours);
+        int closure = Integer.parseInt(closureHours);
         Date todayDate = Calendar.getInstance().getTime();
-        Date openingDate = null;
-        Date closureDate = null;
-        Log.e("closing", closureHours);
-        try {
-            openingDate = new SimpleDateFormat(FORMAT_HOURS, Locale.CANADA).parse(openingHours);
-            closureDate = new SimpleDateFormat(FORMAT_HOURS, Locale.CANADA).parse(closureHours);
-            Log.e("clsong date", closureDate.toString());
-            Log.e("today", todayDate.toString());
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        if (openingDate != null && closureDate != null) {
-            if (todayDate.after(openingDate) && todayDate.before(closureDate)) {
-                long timeBeforeClosure = TimeUnit.MILLISECONDS.toMinutes(Math.abs(todayDate.getTime() - closureDate.getTime()));
-                if(timeBeforeClosure <= 60){
-                    return R.string.closing_soon;
-                } else {
-                    return R.string.open_until;
-                }
-
-            } 
-            if (openingDate.before(todayDate)){
-                long timeBeforeOpening = TimeUnit.MILLISECONDS.toMinutes(Math.abs(openingDate.getTime() - todayDate.getTime()));
-                if(timeBeforeOpening <= 60){
-                    return R.string.opening_soon;
-                } 
-            }
-            
-            return R.string.closed;
-        }
+        DateFormat dateFormat = new SimpleDateFormat(FORMAT_HOURS);
+        String todayDateString = dateFormat.format(todayDate);
+        int todayInt = Integer.parseInt(todayDateString);
         
-        return R.string.no_time;
+        if(opening == closure){
+            return R.string.open_24h;
+        }
 
+        if (todayInt >= opening && todayInt <= closure) {
+            int timeBeforeClosure = closure - todayInt;
+            if(timeBeforeClosure <= 100){
+                return R.string.closing_soon;
+            } else {
+                return R.string.open_until;
+            }
+        }
+        if (todayInt < opening){
+            int timeBeforeOpening = opening - todayInt;
+            if(timeBeforeOpening <= 100){
+                return R.string.opening_soon;
+            }
+        }
 
+        return R.string.closed;
+    }
+
+    public static Date convertStringInDate(String hour){
+        DateFormat dateFormat = new SimpleDateFormat(FORMAT_HOURS);
+        try {
+            return dateFormat.parse(hour);
+        } catch (ParseException e) {
+            return null;
+        }
     }
 }
