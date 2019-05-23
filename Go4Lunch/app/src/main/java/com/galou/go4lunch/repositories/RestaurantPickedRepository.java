28ac5416dd43b298e22a65386064823d641c7454
Galou Minisini <galou.minisini@gmail.com>
2019-05-23 10:26:06 -0700

    Create RestaurantPickedRepository.java

---
+++ Go4Lunch/app/src/main/java/com/galou/go4lunch/repositories/RestaurantPickedRepository.java
@@ -0,0 +1,56 @@
+package com.galou.go4lunch.repositories;
+
+public class RestaurantPickedRepository {
+
+    private static final String COLLECTION_NAME = "restaurantPicked";
+    private CollectionReference restaurantPickedCollection;
+    private RestaurantPicked restaurantPicked;
+
+    private static volatile RestaurantPickedRepository INSTANCE;
+
+    public static RestaurantPickedRepository getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new RestaurantPickedRepository();
+        }
+        return INSTANCE;
+    }
+
+    private RestaurantPickedRepository(){
+        this.restaurantPickedCollection = getrestaurantPickedCollection();
+
+    }
+
+    private CollectionReference getrestaurantPickedCollection(){
+        return FirebaseFirestore.getInstance().collection(COLLECTION_NAME);
+    }
+
+    //---- CREATE ----
+    public Task<Void> createRestarant(String uid, String name, String phtoUrl, String address, int rating, String website, String phoneNumber, String datePicked){
+        restaurantPicked = new RestaurantPicked(uid, name, phtoUrl, address, rating, website, phoneNumber, datePicked);
+        return restaurantPickedCollection.document(uid).set(restaurantPicked);
+    }
+
+    //---- GET ----
+    public RestaurantPicked getRestaurant(){
+        return restaurantPicked;
+    }
+
+    public Task<DocumentSnapshot> getRestaurantFromFirebase(String uid){
+        return restaurantPickedCollection.document(uid).get();
+
+    }
+
+    public Task<QuerySnapshot> getAllRestaurantsFromFirebase(){
+        return restaurantPickedCollection.orderBy("name").get();
+    }
+
+    //---- DELETE ----
+    public Task<Void> deleteRestaurant(String uid){
+        return restaurantPickedCollection.document(uid).delete();
+    }
+
+    public void updateUserRepository(User user){
+        this.user = user;
+    }
+
+}
